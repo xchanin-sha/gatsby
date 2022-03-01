220ad740b64671f4f3f9ceef753db9cf7d07ec84
xchanin <shannonwbruns@gmail.com>
2022-03-01 09:53:28 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://legacyapi.fathym.com/graphql"
+    }
+  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
